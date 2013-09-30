dfda5d92abbe37bc1d4a783182f9828e876d69ce
risk danger olson <technoweenie@gmail.com>
2013-09-30 15:37:03 -0600

    rename struct to Client

--- passthrough.go
+++ passthrough.go
@@ -6,25 +6,25 @@ import (
 	"strconv"
 )
 
-type HttpPassthrough struct {
+type Client struct {
 	Headers []string
 }
 
-func New(headers []string) *HttpPassthrough {
-	return &HttpPassthrough{headers}
+func New(headers []string) *Client {
+	return &Client{headers}
 }
 
-func (p *HttpPassthrough) Pass(res *http.Response, w http.ResponseWriter, status int) {
-	head := p.PassHeaders(res.Header, w)
+func (c *Client) Pass(res *http.Response, w http.ResponseWriter, status int) {
+	head := c.PassHeaders(res.Header, w)
 	head.Set("Content-Length", strconv.Itoa(int(res.ContentLength)))
 	w.WriteHeader(status)
 	io.Copy(w, res.Body)
 	res.Body.Close()
 }
 
-func (p *HttpPassthrough) PassHeaders(resHeader http.Header, w http.ResponseWriter) http.Header {
+func (c *Client) PassHeaders(resHeader http.Header, w http.ResponseWriter) http.Header {
 	head := w.Header()
-	for _, header := range p.Headers {
+	for _, header := range c.Headers {
 		if value := resHeader.Get(header); len(value) != 0 {
 			head.Set(header, value)
 		}
